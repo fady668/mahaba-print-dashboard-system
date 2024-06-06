36c28b9cbbfd92927f00a5afbeafbea06f5f11be
fady668 <fadygeorge668@gmail.com>
2024-06-06 17:15:15 +0300

    start Main Page

--- frontend/src/Pages/Main.jsx
+++ frontend/src/Pages/Main.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Main = () => {
-  return (
-    <div className="main-page">
-      <h1 className="page-title">الصفحه الرئيسيه</h1>
-    </div>
-  );
+    const [totalCash, setTotalCash] = useState("");
+    const [totalReceivedCash, setTotalReceivedCash] = useState("");
+    const [totalRemainingCash, setTotalRemainingCash] = useState("");
+    const [clients, setClients] = useState([]);
+    const [invoises, setInvoises] = useState([]);
+    const [additionals, setAdditionals] = useState([]);
+    const [receivedCash, setReceivedCash] = useState([]);
+
+    return (
+        <div className="main-page">
+            <h1 className="page-title">الصفحه الرئيسيه</h1>
+            <div className="grid">
+                <div className="total-invoises-cash">
+                    <h2>اجمالي حساب التعاملات</h2>
+                </div>
+                <div className="cash-sec">
+                    <div className="total-received-cash"></div>
+                    <div className="total-remaining-cash"></div>
+                </div>
+                <div className="clients-sec"></div>
+                <div className="invoises-sec"></div>
+                <div className="additionals-sec"></div>
+                <div className="received-cash-sec"></div>
+            </div>
+        </div>
+    );
 };
 
 export default Main;
