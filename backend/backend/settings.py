d65ba1d25317fdb8fa3c0331d42c40f4aceef49d
fady668 <fadygeorge668@gmail.com>
2024-06-09 23:21:48 +0300

    The deployment Editions2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,6 +14,13 @@ from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
 import secrets
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+app = get_wsgi_application()
+
 
 load_dotenv()
 
