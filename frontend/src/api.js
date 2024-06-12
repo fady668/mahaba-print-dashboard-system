ef8d802a67a6453e9499d9eb895cb61cd4039401
fady668 <fadygeorge668@gmail.com>
2024-06-12 12:29:20 +0300

    Fix some issues

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import { ACCESS_TOKEN } from "./Constants";
 
 const api = axios.create({
-<<<<<<< HEAD
     baseURL: "http://localhost:8000/",
 });
 
@@ -17,22 +16,6 @@ api.interceptors.request.use(
     (error) => {
         return Promise.reject(error);
     }
-=======
-  baseURL: "https://mahbaprintdashboardsystem-e3f57b0737df.herokuapp.com/",
-});
-
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem(ACCESS_TOKEN);
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
->>>>>>> a1cb958da27acc1351a48af96cb1818e4fe5c5ca
 );
 
 export default api;
